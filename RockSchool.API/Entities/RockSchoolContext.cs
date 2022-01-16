1bcabbb1cde727352092b28d3c3668f53bc04769
t2k-dev <t2k.devel@gmail.com>
2022-01-16 14:30:53 +0600

    Seedidng UserRoles

--- RockSchool.API/Entities/RockSchoolContext.cs
+++ RockSchool.API/Entities/RockSchoolContext.cs
@@ -13,20 +13,20 @@ namespace RockSchool.API.Entities
         }
 
         public DbSet<Discipline> Disciplines { get; set; }
-
         public DbSet<Teacher> Teachers { get; set; }
-
         public DbSet<Schedule> Schedules { get; set; }
-
         public DbSet<Student> Students { get; set; }
-
         public DbSet<Room> Rooms { get; set; }
-
         public DbSet<Attendance> Attendances { get; set; }
-
-        public DbSet<Role> Role { get; set; }
-
+        public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role { RoleId = 1, RoleName = "Admin" },
+                new Role { RoleId = 2, RoleName = "Teacher" },
+                new Role { RoleId = 3, RoleName = "Student" });
+        }
     }
 }
--- RockSchool.API/Entities/Role.cs
+++ RockSchool.API/Entities/Role.cs
@@ -7,7 +7,7 @@ namespace RockSchool.API.Entities
 {
     public class Role
     {
-        public int Id { get; set; }
+        public int RoleId { get; set; }
         public string RoleName { get; set; }
     }
 }
